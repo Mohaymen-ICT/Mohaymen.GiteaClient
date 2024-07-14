ee9c0837fb4d9d4f94d198733d9f15565d38b8d2
HamedSY <hamed007.saboor@gmail.com>
2024-07-14 15:52:52 +0330

    chore: update dto fields

--- Mohaymen.GiteaClient/Gitea/Branch/CreateBranch/Dtos/CreateBranchCommandDto.cs
+++ Mohaymen.GiteaClient/Gitea/Branch/CreateBranch/Dtos/CreateBranchCommandDto.cs
@@ -2,6 +2,8 @@
 
 public class CreateBranchCommandDto
 {
+    public string Owner { get; init; }
+    public string RepositoryName { get; init; }
     public string NewBranchName { get; init; }
     public string OldReferenceName { get; init; }
 }
