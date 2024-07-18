5cef8c41e875cf08fac3cdc0e028b975fcd180d1
alirezaeiji <alirezaeiji191379@gmail.com>
2024-07-18 18:12:09 +0330

    chore: dummy commit

--- Mohaymen.GiteaClient.IntegrationTests/Gitea/Repository/CreateRepository/CreateRepositoryTests.cs
+++ Mohaymen.GiteaClient.IntegrationTests/Gitea/Repository/CreateRepository/CreateRepositoryTests.cs
@@ -35,7 +35,7 @@ public class CreateRepositoryTests
         var actual = await _sut.RepositoryClient.CreateRepositoryAsync(createRepositoryCommandDto, _giteaCollectionFixture.CancellationToken);
 
         // Assert
-        actual.StatusCode.Should().Be(HttpStatusCode.Created);
+        actual.StatusCode.Should().Be(HttpStatusCode.OK);
         actual.Content!.RepositoryName.Should().Be(repositoryName);
     }
 
