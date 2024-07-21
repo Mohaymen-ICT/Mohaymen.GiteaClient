01fe589c6446933efa75b89a2698812ef6e2890f
alirezaeiji <alirezaeiji191379@gmail.com>
2024-07-21 16:18:38 +0330

    add mappers for models

---
+++ Mohaymen.GiteaClient/Gitea/Commit/CreateCommit/Mappers/FileCommitCommandMapper.cs
@@ -0,0 +1,12 @@
+﻿using Mohaymen.GiteaClient.Gitea.Commit.CreateCommit.Commands;
+using Mohaymen.GiteaClient.Gitea.Commit.CreateCommit.Dtos.Request;
+using Riok.Mapperly.Abstractions;
+
+namespace Mohaymen.GiteaClient.Gitea.Commit.CreateCommit.Mappers;
+
+[Mapper(EnumMappingStrategy = EnumMappingStrategy.ByName)]
+internal static partial class FileCommitCommandMapper
+{
+    [MapProperty(nameof(FileCommitDto.CommitActionDto), nameof(FileCommitCommand.CommitActionCommand))]
+    public static partial FileCommitCommand Map(FileCommitDto fileCommitDto);
+}
