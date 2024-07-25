2b17bb3565254fb4aca7fb9527385d12b32e9c86
HamedSY <hamed007.saboor@gmail.com>
2024-07-25 13:38:30 +0330

    fix: integration tests

--- Mohaymen.GiteaClient.IntegrationTests/Gitea/PullRequest/CreatePullRequest/CreatePullRequestTests.cs
+++ Mohaymen.GiteaClient.IntegrationTests/Gitea/PullRequest/CreatePullRequest/CreatePullRequestTests.cs
@@ -4,19 +4,24 @@ using Microsoft.Extensions.DependencyInjection;
 using Mohaymen.GiteaClient.Gitea.Client.Abstractions;
 using Mohaymen.GiteaClient.Gitea.PullRequest.CreatePullRequest.Dtos;
 using Mohaymen.GiteaClient.IntegrationTests.Common.Collections.Gitea;
+using Mohaymen.GiteaClient.IntegrationTests.Common.Initializers.TestData.Abstractions;
 using Mohaymen.GiteaClient.IntegrationTests.Common.Models;
 
 namespace Mohaymen.GiteaClient.IntegrationTests.Gitea.PullRequest.CreatePullRequest;
 
 [Collection("GiteaIntegrationTests")]
-public class CreatePullRequestTests : IClassFixture<CreatePullRequestTestsClassFixture>
+public class CreatePullRequestTests
 {
     private readonly IGiteaClient _sut;
+    private readonly ITestRepositoryCreator _repositoryCreator;
+    private readonly ITestBranchCreator _branchCreator;
     private readonly GiteaCollectionFixture _giteaCollectionFixture;
     
     public CreatePullRequestTests(GiteaCollectionFixture giteaCollectionFixture)
     {
         _giteaCollectionFixture = giteaCollectionFixture ?? throw new ArgumentNullException(nameof(giteaCollectionFixture));
+        _repositoryCreator = _giteaCollectionFixture.ServiceProvider.GetRequiredService<ITestRepositoryCreator>();
+        _branchCreator = _giteaCollectionFixture.ServiceProvider.GetRequiredService<ITestBranchCreator>();
         _sut = giteaCollectionFixture.ServiceProvider.GetRequiredService<IGiteaClient>();
     }
 
@@ -24,17 +29,24 @@ public class CreatePullRequestTests : IClassFixture<CreatePullRequestTestsClassF
     public async Task CreatePullRequest_ShouldGetBranchListOfRepo_WhenInputsAreProvidedProperly()
     {
         // Arrange
+        const string repositoryName = GiteaTestConstants.RepositoryName;
+        const string branchName = "test_branch";
+        var cancellationToken = _giteaCollectionFixture.CancellationToken;
+
+        await _repositoryCreator.CreateRepositoryAsync(repositoryName, cancellationToken);
+        await _branchCreator.CreateBranchAsync(repositoryName, branchName, cancellationToken);
+        
         const string title = "title";
         var createPullRequestCommandDto = new CreatePullRequestCommandDto
         {
-            RepositoryName = GiteaTestConstants.RepositoryName,
+            RepositoryName = repositoryName,
             Title = title,
-            HeadBranch = CreatePullRequestTestsClassFixture.BranchName,
+            HeadBranch = branchName,
             BaseBranch = GiteaTestConstants.DefaultBranch
         };
 
         // Act
-        var actual = await _sut.PullRequestClient.CreatePullRequestAsync(createPullRequestCommandDto, _giteaCollectionFixture.CancellationToken);
+        var actual = await _sut.PullRequestClient.CreatePullRequestAsync(createPullRequestCommandDto, cancellationToken);
 
         // Assert
         actual.StatusCode.Should().Be(HttpStatusCode.Created);
