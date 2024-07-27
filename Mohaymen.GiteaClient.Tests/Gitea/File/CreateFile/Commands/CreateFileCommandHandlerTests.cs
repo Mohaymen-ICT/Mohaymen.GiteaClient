3f4f86dd2667f623cbd1ea085818cca60db9a8dc
HamedSY <hamed007.saboor@gmail.com>
2024-07-27 15:05:55 +0330

    test: unit tests

---
+++ Mohaymen.GiteaClient.Tests/Gitea/File/CreateFile/Commands/CreateFileCommandHandlerTests.cs
@@ -0,0 +1,96 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Options;
+using Mohaymen.GiteaClient.Core.Configs;
+using Mohaymen.GiteaClient.Gitea.File.Common.ApiCall.Abstractions;
+using Mohaymen.GiteaClient.Gitea.File.CreateFile.Commands;
+using Mohaymen.GiteaClient.Gitea.File.CreateFile.Context;
+using Mohaymen.GiteaClient.Gitea.File.CreateFile.Dtos;
+using Mohaymen.GiteaClient.Gitea.File.CreateFile.Models;
+using NSubstitute;
+using Refit;
+using Xunit;
+
+namespace Mohaymen.GiteaClient.Tests.Gitea.File.CreateFile.Commands;
+
+public class CreateFileCommandHandlerTests
+{
+    private readonly IFileRestClient _fileRestClient;
+    private readonly IOptions<GiteaApiConfiguration> _options;
+    private readonly InlineValidator<CreateFileCommand> _validator;
+    private readonly IRequestHandler<CreateFileCommand, ApiResponse<CreateFileResponseDto>> _sut;
+
+    public CreateFileCommandHandlerTests()
+    {
+        _fileRestClient = Substitute.For<IFileRestClient>();
+        _options = Substitute.For<IOptions<GiteaApiConfiguration>>();
+        _validator = new InlineValidator<CreateFileCommand>();
+        _sut = new CreateFileCommandHandler(_fileRestClient, _options, _validator);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowsValidationException_WhenInputIsInvalid()
+    {
+        // Arrange
+        _validator.RuleFor(x => x).Must(_ => false);
+        var command = new CreateFileCommand
+        {
+            RepositoryName = "repo",
+            FilePath = "path",
+            Content = "content"
+        };
+
+        // Act
+        var actual = async () => await _sut.Handle(command, default);
+
+        // Assert
+        await actual.Should().ThrowAsync<ValidationException>();
+    }
+
+    [Fact]
+    public async Task Handle_ShouldCallCreateFileAsync_AndInputsAreValid()
+    {
+        // Arrange
+        _validator.RuleFor(x => x).Must(_ => true);
+        const string owner = "owner";
+        const string repositoryName = "repo";
+        const string filePath = "file_path";
+        const string content = "content";
+        const string branchName = "branch";
+        const string commitMessage = "commit";
+        var identity = new Identity
+        {
+            Email = "email",
+            Name = "name"
+        };
+        var command = new CreateFileCommand
+        {
+            RepositoryName = repositoryName,
+            FilePath = filePath,
+            Content = content,
+            Author = identity,
+            BranchName = branchName,
+            CommitMessage = commitMessage
+        };
+        _options.Value.Returns(new GiteaApiConfiguration
+        {
+            BaseUrl = "url",
+            PersonalAccessToken = "token",
+            RepositoriesOwner = owner
+        });
+
+        // Act
+        await _sut.Handle(command, default);
+        
+        // Assert
+        await _fileRestClient.Received(1).CreateFileAsync(owner, 
+            repositoryName, 
+            filePath, 
+            Arg.Is<CreateFileRequest>(x => x.Content == content
+                                           && x.Author == identity
+                                           && x.BranchName == branchName
+                                           && x.CommitMessage == commitMessage),
+            default);
+    }
+}
---
+++ Mohaymen.GiteaClient.Tests/Gitea/File/CreateFile/Validators/CreateFileCommandValidatorTests.cs
@@ -0,0 +1,101 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using Mohaymen.GiteaClient.Gitea.File.CreateFile.Commands;
+using Mohaymen.GiteaClient.Gitea.File.CreateFile.Validators;
+using Xunit;
+
+namespace Mohaymen.GiteaClient.Tests.Gitea.File.CreateFile.Validators;
+
+public class CreateFileCommandValidatorTests
+{
+    private readonly IValidator<CreateFileCommand> _sut;
+
+    public CreateFileCommandValidatorTests()
+    {
+        _sut = new CreateFileCommandValidator();
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void Validate_ShouldReturnEmptyRepositoryNameErrorCode_WhenRepositoryNameIsNullOrEmpty(string repositoryName)
+    {
+        // Arrange
+        var command = new CreateFileCommand
+        {
+            RepositoryName = repositoryName,
+            FilePath = "filePath",
+            Content = "content"
+        };
+
+        // Act
+        var actual = _sut.Validate(command);
+
+        // Assert
+        actual.IsValid.Should().BeFalse();
+        actual.Errors.Select(x => x.ErrorCode)
+            .Should().Contain(CreateFileErrorCodes.EmptyRepositoryNameErrorCode);
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void Validate_ShouldReturnEmptyFilePathErrorCode_WhenFilePathIsNullOrEmpty(string filePath)
+    {
+        // Arrange
+        var command = new CreateFileCommand
+        {
+            RepositoryName = "repo",
+            FilePath = filePath,
+            Content = "content"
+        };
+
+        // Act
+        var actual = _sut.Validate(command);
+
+        // Assert
+        actual.IsValid.Should().BeFalse();
+        actual.Errors.Select(x => x.ErrorCode)
+            .Should().Contain(CreateFileErrorCodes.EmptyFilePathErrorCode);
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void Validate_ShouldReturnEmptyContentErrorCode_WhenFilePathIsNullOrEmpty(string content)
+    {
+        // Arrange
+        var command = new CreateFileCommand
+        {
+            RepositoryName = "repo",
+            FilePath = "path",
+            Content = content
+        };
+
+        // Act
+        var actual = _sut.Validate(command);
+
+        // Assert
+        actual.IsValid.Should().BeFalse();
+        actual.Errors.Select(x => x.ErrorCode)
+            .Should().Contain(CreateFileErrorCodes.EmptyContentErrorCode);
+    }
+    
+    [Fact]
+    public void Validate_ShouldReturnValidResult_WhenEveryThingIsProvidedProperly()
+    {
+        // Arrange
+        var command = new CreateFileCommand
+        {
+            RepositoryName = "repo",
+            FilePath = "path",
+            Content = "content"
+        };
+
+        // Act
+        var actual = _sut.Validate(command);
+        
+        // Assert
+        actual.IsValid.Should().BeTrue();
+    }
+}
