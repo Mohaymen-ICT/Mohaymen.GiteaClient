b0f16bd967be8781bb9cdafd89f9b8c0a7c83d96
HamedSY <hamed007.saboor@gmail.com>
2024-07-30 15:36:13 +0330

    fix: change some optional fields to required

--- Mohaymen.GiteaClient/Gitea/PullRequest/Common/Models/GiteaBranch.cs
+++ Mohaymen.GiteaClient/Gitea/PullRequest/Common/Models/GiteaBranch.cs
@@ -5,5 +5,5 @@ namespace Mohaymen.GiteaClient.Gitea.PullRequest.Common.Models;
 public class GiteaBranch
 {
     [JsonProperty("label")] 
-    public string? Name { get; init; }
+    public required string Name { get; init; }
 }
--- Mohaymen.GiteaClient/Gitea/PullRequest/GetPullRequestList/Dtos/GetPullRequestListResponseDto.cs
+++ Mohaymen.GiteaClient/Gitea/PullRequest/GetPullRequestList/Dtos/GetPullRequestListResponseDto.cs
@@ -7,17 +7,17 @@ namespace Mohaymen.GiteaClient.Gitea.PullRequest.GetPullRequestList.Dtos;
 public class GetPullRequestListResponseDto
 {
     [JsonProperty("title")] 
-    public string? Title { get; init; }
-    
+    public required string Title { get; init; }
+
+    [JsonProperty("head")] 
+    public required GiteaBranch HeadBranch { get; init; }
+
+    [JsonProperty("base")] 
+    public required GiteaBranch BaseBranch { get; init; }
+
     [JsonProperty("body")] 
     public string? Body { get; init; }
-    
-    [JsonProperty("head")] 
-    public GiteaBranch? HeadBranch { get; init; }
-    
-    [JsonProperty("base")] 
-    public GiteaBranch? BaseBranch { get; init; }
-    
+
     [JsonProperty("assignees")] 
     public List<User> Assignees { get; init; } = [];
     
