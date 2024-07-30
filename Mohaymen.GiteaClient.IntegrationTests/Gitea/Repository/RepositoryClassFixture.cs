e5645d2d2e14e98ac4db01aeebf29e24d0a654d1
Alireza Eiji <alirezaeiji191379@gmail.com>
2024-07-30 15:00:37 +0330

    correct the build

--- Mohaymen.GiteaClient.IntegrationTests/Gitea/Repository/RepositoryClassFixture.cs
+++ Mohaymen.GiteaClient.IntegrationTests/Gitea/Repository/RepositoryClassFixture.cs
@@ -10,16 +10,18 @@ internal sealed class RepositoryClassFixture : IAsyncLifetime
     public const string DeleteRepositoryName = "DeleteUseCaseRepository";
     
     private readonly ITestRepositoryCreator _testRepositoryCreator;
+    private readonly GiteaCollectionFixture _giteaCollectionFixture;
 
     public RepositoryClassFixture(GiteaCollectionFixture giteaCollectionFixture)
     {
+        _giteaCollectionFixture = giteaCollectionFixture ?? throw new ArgumentNullException(nameof(giteaCollectionFixture));
         _testRepositoryCreator = giteaCollectionFixture.ServiceProvider.GetRequiredService<ITestRepositoryCreator>();
     }
     
     public async Task InitializeAsync()
     {
-        await _testRepositoryCreator.CreateRepository(SearchRepositoryName);
-        await _testRepositoryCreator.CreateRepository(DeleteRepositoryName);
+        await _testRepositoryCreator.CreateRepositoryAsync(SearchRepositoryName, _giteaCollectionFixture.CancellationToken);
+        await _testRepositoryCreator.CreateRepositoryAsync(DeleteRepositoryName, _giteaCollectionFixture.CancellationToken);
     }
 
     public Task DisposeAsync()
