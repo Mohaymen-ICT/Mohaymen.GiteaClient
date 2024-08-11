cd3686926f7dbc7b64a794164f329758277a92b1
alireza hajli <hajlialireza@gmail.com>
2024-08-11 19:21:12 +0330

    fix: fix expectedCommitMessages pagination

--- Mohaymen.GiteaClient.IntegrationTests/Gitea/Commit/GetBranchCommits/GetBranchCommitsTests.cs
+++ Mohaymen.GiteaClient.IntegrationTests/Gitea/Commit/GetBranchCommits/GetBranchCommitsTests.cs
@@ -13,7 +13,6 @@ public class GetBranchCommitsTests
 {
     private const string RepositoryName = "GetBranchCommitsRepo";
     private const string BranchName = "feature/IntegrationTest";
-    private const string FilePath = "README1.md";
     private const string CommitMessage = "fakeCommitMessage";
     private readonly ITestRepositoryCreator _testRepositoryCreator;
     private readonly ITestBranchCreator _testBranchCreator;
@@ -23,43 +22,50 @@ public class GetBranchCommitsTests
 
     public GetBranchCommitsTests(GiteaCollectionFixture giteaCollectionFixture)
     {
-        _giteaCollectionFixture = giteaCollectionFixture ?? throw new ArgumentNullException(nameof(giteaCollectionFixture));
+        _giteaCollectionFixture =
+            giteaCollectionFixture ?? throw new ArgumentNullException(nameof(giteaCollectionFixture));
         _testRepositoryCreator = giteaCollectionFixture.ServiceProvider.GetRequiredService<ITestRepositoryCreator>();
         _testBranchCreator = giteaCollectionFixture.ServiceProvider.GetRequiredService<ITestBranchCreator>();
         _testCommiter = giteaCollectionFixture.ServiceProvider.GetRequiredService<ITestCommiter>();
         _sut = _giteaCollectionFixture.ServiceProvider.GetRequiredService<IGiteaClient>();
     }
-   
-    public static TheoryData<int, int, List<string>> PageLimitCommitMessages()
+
+    public static TheoryData<string, int, int, List<string>> PageLimitCommitMessages()
     {
-        return new TheoryData<int, int, List<string>>()
+        return new TheoryData<string, int, int, List<string>>()
         {
             {
+                "file1.text",
                 1,
                 1,
                 [$"{CommitMessage}\n"]
             },
             {
+                "file2.text",
                 2,
-                1,
+                2,
                 ["Initial commit\n"]
             },
             {
-                2,
-                2,
-                []
+                "file3.text",
+                1,
+                1,
+                ["test\n"]
             },
             {
+                "file4.text",
                 1,
-                10,
-                [$"{CommitMessage}\n", "Initial commit\n"]
+                4,
+                ["Last commit\n", "test\n", "Initial commit\n", $"{CommitMessage}\n"]
             }
         };
     }
 
     [Theory]
     [MemberData(nameof(PageLimitCommitMessages))]
-    public async Task GetBranchCommitsAsync_ShouldReturnOkAndBranchCommitsBasedOnPageSizeAndLimitSize_WhenPageSizeAndLimitSizeAreSet(int page, int limit, List<string> expectedCommitMessages)
+    public async Task
+        GetBranchCommitsAsync_ShouldReturnOkAndBranchCommitsBasedOnPageSizeAndLimitSize_WhenPageSizeAndLimitSizeAreSet(
+            string filePath, int page, int limit, List<string> expectedCommitMessages)
     {
         // Arrange 
         var loadBranchCommitsDto = new LoadBranchCommitsQueryDto
@@ -70,11 +76,15 @@ public class GetBranchCommitsTests
             Limit = limit
         };
         await _testRepositoryCreator.CreateRepositoryAsync(RepositoryName, _giteaCollectionFixture.CancellationToken);
-        await _testBranchCreator.CreateBranchAsync(RepositoryName, BranchName, _giteaCollectionFixture.CancellationToken);
-        await _testCommiter.CreateFileAsync(RepositoryName, BranchName, FilePath, CommitMessage, _giteaCollectionFixture.CancellationToken);
-        
+        await _testBranchCreator.CreateBranchAsync(RepositoryName, BranchName,
+            _giteaCollectionFixture.CancellationToken);
+        await _testCommiter.CreateFileAsync(RepositoryName, BranchName, filePath, expectedCommitMessages.First(),
+            _giteaCollectionFixture.CancellationToken);
+
         // Act
-        var actual = await _sut.CommitClient.LoadBranchCommitsAsync(loadBranchCommitsDto, _giteaCollectionFixture.CancellationToken);
+        var actual =
+            await _sut.CommitClient.LoadBranchCommitsAsync(loadBranchCommitsDto,
+                _giteaCollectionFixture.CancellationToken);
 
         // Assert
         actual.StatusCode.Should().Be(HttpStatusCode.OK);
