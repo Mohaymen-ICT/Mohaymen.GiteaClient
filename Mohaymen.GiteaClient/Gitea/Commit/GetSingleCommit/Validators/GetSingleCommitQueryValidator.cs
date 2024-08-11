734b1fb0ffc55e5e71915d19c272757b879686a4
alireza hajli <hajlialireza@gmail.com>
2024-08-11 16:42:35 +0330

    feat: add unit test for single commit validator

---
+++ Mohaymen.GiteaClient/Gitea/Commit/GetSingleCommit/Validators/GetSingleCommitQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Mohaymen.GiteaClient.Core.Validation;
+using Mohaymen.GiteaClient.Gitea.Commit.GetBranchCommits.Queries;
+
+namespace Mohaymen.GiteaClient.Gitea.Commit.GetBranchCommits.Validators;
+
+internal sealed class GetSingleCommitQueryValidator : AbstractValidator<GetSingleCommitQuery>
+{
+    public GetSingleCommitQueryValidator()
+    {
+        RuleFor(x => x.RepositoryName)
+            .NotEmpty()
+            .WithErrorCode(ValidationErrorCodes.EmptyRepositoryNameErrorCode)
+            .WithMessage("repository name should not be empty");
+        RuleFor(x => x.CommitSha)
+            .NotEmpty()
+            .WithErrorCode(ValidationErrorCodes.EmptyCommitShaErrorCode)
+            .WithMessage("commit hash(sha) should not be empty");
+    }
+}
