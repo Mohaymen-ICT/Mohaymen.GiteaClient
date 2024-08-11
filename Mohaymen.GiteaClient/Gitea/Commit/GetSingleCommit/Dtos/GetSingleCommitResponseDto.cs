b232e614c0a6c3005d0c3ed7d6b785116114b968
alireza hajli <hajlialireza@gmail.com>
2024-08-11 18:05:49 +0330

    chore: reformat

--- Mohaymen.GiteaClient/Gitea/Commit/GetSingleCommit/Dtos/GetSingleCommitResponseDto.cs
+++ Mohaymen.GiteaClient/Gitea/Commit/GetSingleCommit/Dtos/GetSingleCommitResponseDto.cs
@@ -5,27 +5,36 @@ namespace Mohaymen.GiteaClient.Gitea.Commit.GetBranchCommits.Dtos;
 
 public sealed class GetSingleCommitResponseDto
 {
-    [JsonProperty("url")] public required string Url { get; init; }
+    [JsonProperty("url")] 
+    public required string Url { get; init; }
 
-    [JsonProperty("sha")] public required string CommitSha { get; init; }
+    [JsonProperty("sha")] 
+    public required string CommitSha { get; init; }
 
-    [JsonProperty("files")] public required List<FilesDto> FilesDto { get; init; }
+    [JsonProperty("files")] 
+    public required List<FilesDto> FilesDto { get; init; }
 
-    [JsonProperty("stats")] public required StatsDto StatsDto { get; init; }
+    [JsonProperty("stats")] 
+    public required StatsDto StatsDto { get; init; }
 }
 
 public sealed class FilesDto
 {
-    [JsonProperty("filename")] public required string Filename { get; init; }
+    [JsonProperty("filename")] 
+    public required string Filename { get; init; }
 
-    [JsonProperty("status")] public required string Status { get; init; }
+    [JsonProperty("status")] 
+    public required string Status { get; init; }
 }
 
 public sealed class StatsDto
 {
-    [JsonProperty("total")] public required int Total { get; init; }
+    [JsonProperty("total")] 
+    public required int Total { get; init; }
 
-    [JsonProperty("additions")] public required int Additions { get; init; }
+    [JsonProperty("additions")] 
+    public required int Additions { get; init; }
 
-    [JsonProperty("deletions")] public required int Deletions { get; init; }
+    [JsonProperty("deletions")] 
+    public required int Deletions { get; init; }
 }
