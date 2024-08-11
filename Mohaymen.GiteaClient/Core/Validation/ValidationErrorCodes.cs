766a61c6084d66ae1d4abc864db6962fb94d1e11
alireza hajli <hajlialireza@gmail.com>
2024-08-11 16:35:29 +0330

    feat: add validator

--- Mohaymen.GiteaClient/Core/Validation/ValidationErrorCodes.cs
+++ Mohaymen.GiteaClient/Core/Validation/ValidationErrorCodes.cs
@@ -6,6 +6,7 @@ internal static class ValidationErrorCodes
     internal const string EmptyBranchNameErrorCode = "EmptyBranchNameErrorCode";
     internal const string EmptyCommitMessageErrorCode = "EmptyCommitMessageErrorCode";
     internal const string EmptySearchQueryErrorCode = "EmptySearchQueryErrorCode";
+    internal const string EmptyCommitShaErrorCode = "EmptyCommitShaErrorCode";
     internal const string InvalidPageSizeErrorCode = "InvalidPageSizeErrorCode";
     internal const string InvalidLimitErrorCode = "InvalidLimitErrorCode";
     internal const string InvalidFilePathErrorCode = "InvalidFilePathErrorCode";
