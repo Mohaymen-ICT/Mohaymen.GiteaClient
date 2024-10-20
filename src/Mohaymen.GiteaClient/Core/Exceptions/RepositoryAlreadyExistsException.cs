9b60174b0866044bcbd9f0b2c149e41326260b85
alireza <alirezasamimi1381@gmail.com>
2024-10-20 14:22:45 +0330

    f

---
+++ src/Mohaymen.GiteaClient/Core/Exceptions/RepositoryAlreadyExistsException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Mohaymen.GiteaClient.Core.Exceptions;
+
+public class RepositoryAlreadyExistsException : Exception
+{
+    public RepositoryAlreadyExistsException()
+    {
+    }
+
+    public RepositoryAlreadyExistsException(string message) : base(message)
+    {
+    }
+
+    public RepositoryAlreadyExistsException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
